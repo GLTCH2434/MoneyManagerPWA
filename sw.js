1618775e284fa9335be045097224782e46cf8c37
GLTCH2434 <129479462+GLTCH2434@users.noreply.github.com>
2026-07-31 22:47:48 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,44 @@
-const CACHE = "money-manager-v1";
+const CACHE_NAME = "money-manager-v2";
 
-const FILES = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json"
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE).then(cache => cache.addAll(FILES))
-    );
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    clients.claim()
+  );
+
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => response || fetch(event.request))
-    );
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
 });
